181d4422994f80ed9f8b4823ecf08a4bea98b2ed
unknown <NamGwonPyo>
2020-04-25 22:32:20 +0900

    2week gwonpyo

---
+++ 소스코드(이름_문제번호)/남권표_1931.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int **time;
+	int N;
+	cin >> N;
+	time = new int*[N];
+	int trash = 0;
+	for (int i = 0; i < N; i++) {
+		time[i] = new int[2];
+	}
+	for (int i = 0; i < N; i++) {
+		cin >> time[i][1] >> time[i][2];
+	}
+	while (true) {
+		for (int i = 0; i < N; i++) {
+			if()
+		}
+	}
+}
